3561c034fad18bc2d661e8ab3d8891d9caf255bb
Amitkumudu <amit@kumudu.in>
2025-10-10 12:38:38 +0530

    first commit

---
+++ 111-sai gold -DVG - QR/invoice/dsinvoice.vb
@@ -0,0 +1,11 @@
+﻿Partial Class dsinvoice
+    Partial Class rptheaderDataTable
+
+        Private Sub rptheaderDataTable_ColumnChanging(ByVal sender As System.Object, ByVal e As System.Data.DataColumnChangeEventArgs) Handles Me.ColumnChanging
+            'End If
+
+        End Sub
+
+    End Class
+
+End Class
